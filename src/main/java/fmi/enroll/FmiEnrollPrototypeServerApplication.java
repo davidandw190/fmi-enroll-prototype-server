617653fb24555fbc6e78e2df4f1fa74584fa087f
Andrei-David Nan <nandavid190@gmail.com>
2024-12-27 20:43:36 +0200

    Initial commit

---
+++ src/main/java/fmi/enroll/FmiEnrollPrototypeServerApplication.java
@@ -0,0 +1,13 @@
+package fmi.enroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmiEnrollPrototypeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmiEnrollPrototypeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fmi/enroll/FmiEnrollPrototypeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fmi.enroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmiEnrollPrototypeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
